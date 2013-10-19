76db70dbee0d8a91373222df2113683abd8d1743
Ian Scott <ian@polpo.org>
2013-10-19 00:07:31 -0700

    Update to use Target's newer JSON-based inventory checking; update list of iPad models; prettify output a little bit.

--- ipad-target.py
+++ ipad-target.py
@@ -7,31 +7,68 @@ import smtplib
 from email.mime.text import MIMEText
 import sys
 
-url = 'http://www.target.com/FiatsCmd?'
+import json as json
 
 mode = sys.argv[1]
 zip = sys.argv[2]
 dpcis = sys.argv[3:]
 
 models = {
-    '057-10-2020': ('204704844','14000303','16GB The new iPad with Wi-Fi - Black (MC705LL/A)'),
-    '057-10-2021': ('204704865','14000304','16GB The new iPad with Wi-Fi - White (MD328LL/A)'),
-    '057-10-2022': ('204704833','13999147','32GB The new iPad with Wi-Fi - Black (MC706LL/A)'),
-    '057-10-2023': ('204704858','13999148','32GB The new iPad with Wi-Fi - White (MD329LL/A)'),
-    '057-10-2024': ('204704877','13999149','64GB The new iPad with Wi-Fi - Black (MC707LL/A)'),
-    '057-10-2025': ('204704934','13999150','64GB The new iPad with Wi-Fi - White (MD330LL/A)'),
-    '057-10-2026': ('204704892','13999151','16GB The new iPad with Wi-Fi + 4G for AT&T - Black (MD366LL/A)'),
-    '057-10-2027': ('204704916','13999152','16GB The new iPad with Wi-Fi + 4G for AT&T - White (MD369LL/A)'),
-    '057-10-2028': ('204704936','13999153','32GB The new iPad with Wi-Fi + 4G for AT&T - Black (MD367LL/A)'),
-    '057-10-2029': ('204704924','13999154','32GB The new iPad with Wi-Fi + 4G for AT&T - White (MD370LL/A)'),
-    '057-10-2030': ('204704940','13999155','64GB The new iPad with Wi-Fi + 4G for AT&T - Black (MD368LL/A)'),
-    '057-10-2031': ('204704904','13999156','64GB The new iPad with Wi-Fi + 4G for AT&T - White (MD371LL/A)'),
-    '057-10-2032': ('204704928','13999157','16GB The new iPad with Wi-Fi + 4G for Verizon - Black (MC733LL/A)'),
-    '057-10-2033': ('204704943','13999158','16GB The new iPad with Wi-Fi + 4G for Verizon - White (MD363LL/A)'),
-    '057-10-2034': ('204704906','13999159','32GB The new iPad with Wi-Fi + 4G for Verizon - Black (MC744LL/A)'),
-    '057-10-2036': ('204785282','14038112','32GB The new iPad with Wi-Fi + 4G for Verizon - White (MD364LL/A)'),
-    '057-10-2037': ('204785234','14038213','64GB The new iPad with Wi-Fi + 4G for Verizon - Black (MC756LL/A)'),
-    '057-10-2038': ('204785257','14038214','64GB The new iPad with Wi-Fi + 4G for Verizon - White (MD365LL/A)'),
+    # Currently sold in stores
+    '057-10-0172': 'Apple iPad mini 16GB Wi-Fi - Black (MD528LL/A)',
+    '057-10-0173': 'Apple iPad mini 16GB Wi-Fi - White (MD531LL/A)',
+    '057-10-0174': 'Apple iPad mini 32GB Wi-Fi - Black (MD529LL/A)',
+    '057-10-0175': 'Apple iPad mini 32GB Wi-Fi - White (MD532LL/A)',
+    '057-10-1830': 'Apple 16GB iPad 2 with Wi-Fi - Black (MC954LL/A)',
+    '057-10-1839': 'Apple 16GB iPad 2 with Wi-Fi - White (MC989LL/A)',
+    '057-10-2024': 'Apple 64GB iPad with Wi-Fi (3rd generation) - Black (MC707LL/A)',
+    '057-10-2026': 'Apple 16GB iPad with Wi-Fi (3rd generation) + 4G for AT&T - Black (MD366LL/A)',
+    '057-10-2031': 'Apple 64GB iPad with Wi-Fi (3rd generation) + 4G for AT&T - White (MD371LL/A)',
+    '057-10-2053': 'Apple 32GB iPad with Retina display - White (MD514LL/A)',
+    '057-10-2054': 'Apple 64GB iPad with Retina display - Black (MD512LL/A)',
+    '057-10-2055': 'Apple 64GB iPad with Retina display - White (MD515LL/A)',
+
+    # Not sold in stores (not an exhaustive list)
+    '057-10-2020': 'Apple 16GB iPad with Wi-Fi (3rd generation) - Black (MC705LL/A)',
+    '057-10-2021': 'Apple 16GB iPad with Wi-Fi (3rd generation) - White (MD328LL/A)',
+    '057-10-2022': 'Apple 32GB iPad with Wi-Fi (3rd generation) - Black (MC706LL/A)',
+    '057-10-2023': 'Apple 32GB iPad with Wi-Fi (3rd generation) - White (MD329LL/A)',
+    '057-10-2025': 'Apple 64GB iPad with Wi-Fi (3rd generation) - White (MD330LL/A)',
+    '057-10-2027': 'Apple 16GB iPad with Wi-Fi (3rd generation) + 4G for AT&T - White (MD369LL/A)',
+    '057-10-2028': 'Apple 32GB iPad with Wi-Fi (3rd generation) + 4G for AT&T - Black (MD367LL/A)',
+    '057-10-2029': 'Apple 32GB iPad with Wi-Fi (3rd generation) + 4G for AT&T - White (MD370LL/A)',
+    '057-10-2030': 'Apple 64GB iPad with Wi-Fi (3rd generation) + 4G for AT&T - Black (MD368LL/A)',
+    '057-10-2032': 'Apple 16GB iPad with Wi-Fi (3rd generation) + 4G for Verizon - Black (MC733LL/A)',
+    '057-10-2033': 'Apple 16GB iPad with Wi-Fi (3rd generation) + 4G for Verizon - White (MD363LL/A)',
+    '057-10-2034': 'Apple 32GB iPad with Wi-Fi (3rd generation) + 4G for Verizon - Black (MC744LL/A)',
+    '057-10-2036': 'Apple 32GB iPad with Wi-Fi (3rd generation) + 4G for Verizon - White (MD364LL/A)',
+    '057-10-2037': 'Apple 64GB iPad with Wi-Fi (3rd generation) + 4G for Verizon - Black (MC756LL/A)',
+    '057-10-2038': 'Apple 64GB iPad with Wi-Fi (3rd generation) + 4G for Verizon - White (MD365LL/A)',
+
+    '057-10-0183': 'Apple 32GB iPad with Retina display Wi-Fi + Cellular (AT&T) - Black (MD517LL/A)',
+    '057-10-0184': 'Apple 32GB iPad with Retina display Wi-Fi + Cellular (Verizon - Black (MD523LL/A)',
+    '057-10-0185': 'Apple 32GB iPad with Retina display Wi-Fi + Cellular (Sprint - Black (ME196LL/A)',
+    '057-10-2050': 'Apple 16GB iPad with Retina display - Black (MD510LL/A)',
+    '057-10-2051': 'Apple 16GB iPad with Retina display - White (MD513LL/A)',
+    '057-10-2052': 'Apple 32GB iPad with Retina display - Black (MD511LL/A)',
+    '241-27-2248': 'Apple 16GB iPad with Retina display Wi-Fi + Cellular (AT&T) - Black (MD516LL/A)',
+    '241-27-2250': 'Apple 16GB iPad with Retina display Wi-Fi + Cellular (AT&T) - White (MD519LL/A)',
+    '241-27-2256': 'Apple 32GB iPad with Retina display Wi-Fi + Cellular (Verizon)- White (MD526LL/A)',
+    '241-27-2257': 'Apple 64GB iPad with Retina display Wi-Fi + Cellular (Verizon) - White (MD527LL/A)',
+    '241-27-2259': 'Apple 64GB iPad with Retina display Wi-Fi + Cellular (Sprint)- Black (ME197LL/A)',
+    '241-27-2263': 'Apple 32GB iPad with Retina display Wi-Fi + Cellular (AT&T) - White (MD520LL/A)',
+    '241-27-4384': 'Apple 128GB iPad with Retina display - Black (ME392LL/A)',
+    '241-27-4385': 'Apple 128GB iPad with Retina display - White (ME393LL/A)',
+
+    '057-10-0176': 'Apple iPad mini 64GB Wi-Fi - Black (MD530LL/A)',
+    '057-10-0177': 'Apple iPad mini 64GB Wi-Fi - White (MD533LL/A)',
+    '057-10-0178': 'Apple iPad mini 16GB Wi-Fi + Cellular (AT&T) - Black (MD534LL/A)',
+    '057-10-0180': 'Apple iPad mini 32GB Wi-Fi + Cellular (AT&T) - Black (MD535LL/A)',
+    '057-10-0187': 'Apple iPad mini 32GB Wi-Fi + Cellular (Verizon) - White (MD544LL/A)',
+    '057-10-0189': 'Apple iPad mini 64GB Wi-Fi + Cellular (Verizon) - White (MD545LL/A)',
+    '241-27-2241': 'Apple iPad mini 32GB Wi-Fi + Cellular (AT&T) - White (MD538LL/A)',
+    '241-27-2243': 'Apple iPad mini 16GB Wi-Fi + Cellular (Verizon) - Black (MD540LL/A)',
+    '241-27-2244': 'Apple iPad mini 16GB Wi-Fi + Cellular (Verizon) - White (MD543LL/A)',
 }
 
 results = ""
@@ -40,44 +77,74 @@ stores = {}
 if not dpcis:
     dpcis = models.keys()
 
-for dpci in dpcis:
-    data = urllib.urlencode({
-        'partNumber': models[dpci][1],
-        'catalogEntryID': models[dpci][0],
-        'ValidDpci': dpci,
-        'zipCode': zip,
-        'isSoftRefresh': 'true',
-        'attrName_1': '',
-        'attrName_2': '',
-        'city': '',
-        'state': '',
+jsonurl = 'http://api.target.com/products/v3/saleable_quantity_by_location?key=q0jGNkIyuqUTYIlzZKoCfK6ugaNGSP8h'
+
+def getjsonurl():
+    fiatsurl = 'http://www.target.com/FiatsCmd?'
+    # Get a key to look stuff up with. Use any item get the current key.
+    fiatsdata = urllib.urlencode({
+	'template': 'medium',
+	'overlayId': 'FindinStore',
+	'pageType': 'pdp',
+	'fiatLocation': 'product+detail',
+	'catalogEntryID': '204704844',
+	'partNumber': '14000303',
+	'ValidDpci': '057-10-2020'
     })
 
-    req = urllib2.Request(url, data)
+    req = urllib2.Request(fiatsurl + fiatsdata, '', {'X-Requested-With': 'XMLHttpRequest'})
     response = urllib2.urlopen(req)
     #print response.info()
     page = response.read()
 
     soup = BeautifulSoup(page)
 
-    trs = soup.find_all('tr')
+    jsonurl = soup.find('input', attrs={'id': 'saleableQtyLocURL'})['value']
+    return jsonurl
 
-    for tr in trs[2:]:
-        tds = tr.find_all('td')
-        store = tds[1].span.string
-        address = tds[1].select('.store-address')[0].string
-        status = tds[2].strong.string
-        #print store, status
+def getproductjson(jsonurl):
+	jsondata = json.dumps({
+	    "products": [{
+		"product_id": dpci,
+		"desired_quantity": "1"
+	    } for dpci in dpcis],
+	    "nearby": zip,
+	    "radius": "25",
+	    "multichannel_options":[{
+		"multichannel_option": "none"
+	    }]
+	})
+	req = urllib2.Request(jsonurl, jsondata, {'Content-Type': 'application/json; charset=UTF-8', 'Accept': 'application/json'})
+	response = urllib2.urlopen(req)
+	page = response.read()
+	return page
 
-        if status not in ("out of stock", "not sold at this store"):
-            results += models[dpci][2] + ': ' + store + ': ' + status + '\n'
-            if store not in stores:
-                stores[store] = address
+page = getproductjson(jsonurl)
+
+products = json.loads(page)['products']
+
+for product in products:
+    product_id = product['product_id']
+    # Look up friendly product name
+    if product_id in models:
+        product_name = models[product_id]
+    else:
+        product_name = product_id
+
+    for store in product['stores']:
+        if store['saleable_quantity'] > 0:
+	    if product_name:
+		results += "%s:\n" % (product_name,)
+		product_name = None
+            results += "    %s: %s (Qty: %d)\n" % (store['store_name'], store['availability_status'], store['saleable_quantity'])
+            if store['store_id'] not in stores:
+                stores[store['store_id']] = store
+    results += '\n'
 
 if stores:
-    results += "\nStore addresses:\n"
-    for store in stores:
-        results += store + ": " + ' '.join(stores[store].split()) + "\n"
+    results += 'Store addresses:\n'
+    for store in stores.itervalues():
+        results += "    %s: %s\n" % (store['store_name'], store['store_address'])
     
 if results:
     if mode == 'print':
